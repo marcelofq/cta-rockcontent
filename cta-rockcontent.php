a2cd2a0b4c26e51de29e6e9cf4dc5e7664f8a982
marcelofq <contato@marcelofq.com.br>
2018-12-11 16:11:06 -0200

    putting all code in a class

--- cta-rockcontent.php
+++ cta-rockcontent.php
@@ -15,49 +15,81 @@ Domain Path:  /cta-rockcontent
 */
 
 // Plugin main function that creates a custom post type called cta-banner and add it to wordpress init action
-add_action( 'init', 'cta_rockcontent_init', 0 );
-function cta_rockcontent_init() {
+class CTA_RockContent {
 
-	$labels = array(
+	const POST_TYPE	= "cta-banner";
+	
+	public function __construct(){		
+		add_action('init', array(&$this, 'init'));
+	}
+
+
+	public function init(){
+		// Adding custom imagem size (cta-banner-size)
+		add_image_size( 'cta-banner-size', 960, 300, true ); // hard crop mode
+
+		//Create new custom post typr called cta-banner
+		$this->create_post_type();
+		
+		//Adding the [cta-banner] shortcode
+		add_shortcode(self::POST_TYPE, array($this, 'cta_rockcontent_shortcode_show_html'));
+
+		//Action to save the metabox value
+		add_action( 'save_post', array($this, 'cta_banner_save_meta' ));
+	}
+	
+
+	/************************* 
+	   Create the post type
+	**************************/
+	public function create_post_type(){
+
+		$labels = array(
 		'name'                  => _x( "CTA's", 'Post Type General Name', 'cta-rockcontent' ),
 		'singular_name'         => _x( 'CTA', 'Post Type Singular Name', 'cta-rockcontent' ),
 		'menu_name'             => __( "CTA's", 'cta-rockcontent' ),
 		'name_admin_bar'        => __( 'CTA', 'cta-rockcontent' )
-	);
-	$args = array(
-		'label'                 => __( 'CTA', 'cta-rockcontent' ),
-		'description'           => __( 'Call to Action', 'cta-rockcontent' ),
-		'labels'                => $labels,
-		'supports'              => array('title', 'thumbnail'),
-		'hierarchical'          => false,
-		'public'                => true,
-		'show_ui'               => true,
-		'show_in_menu'          => true,
-		'menu_position'         => 5,
-		'show_in_admin_bar'     => false,
-		'show_in_nav_menus'     => true,
-		'can_export'            => true,
-		'has_archive'           => true,
-		'exclude_from_search'   => false,
-		'publicly_queryable'    => false,
-		'menu_icon'				=> 'dashicons-admin-comments',
-		'register_meta_box_cb'  => 'cta_banner_add_metabox',
-		'capability_type'       => 'post'
-	);
-	// Registering the custom post type cta-banner
-	register_post_type( 'cta-banner', $args );
+		);
+		$args = array(
+			'label'                 => __( 'CTA', 'cta-rockcontent' ),
+			'description'           => __( 'Call to Action', 'cta-rockcontent' ),
+			'labels'                => $labels,
+			'supports'              => array( 'title', 'thumbnail' ),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'show_in_admin_bar'     => false,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => true,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => false,
+			'menu_icon'				=> 'dashicons-admin-comments',
+			'register_meta_box_cb'  => array( $this, 'cta_banner_add_metabox' ),
+			'capability_type'       => 'post'
+		);
+		// Registering the custom post type cta-banner
+		register_post_type( self::POST_TYPE, $args );
 
-	// Adding custom imagem size (cta-banner-size)
-	add_image_size( 'cta-banner-size', 960, 300, true ); // hard crop mode	
-
-	// Function to add the metabox url banner
-	function cta_banner_add_metabox(){
-		// Adding the URL banner field into custom post type cta-banner
-		add_meta_box( 'cta-banner-url', 'URL', 'cta_banner_url_html', 'cta-banner', 'normal', 'high' );
 	}
 
-	//Callback of add_meta_box 
-	function cta_banner_url_html(){
+
+
+	/******************************************* 
+	   Function to add the metabox url banner
+	********************************************/
+	public function cta_banner_add_metabox(){
+		// Adding the URL banner field into custom post type cta-banner
+		add_meta_box( 'cta-banner-url', 'URL', array( $this, 'cta_banner_url_html'), 'cta-banner', 'normal', 'high' );
+	}
+
+	
+	/***************************************************** 
+	   Function to create URL field on CTA new/edit page
+	******************************************************/
+	public function cta_banner_url_html(){
 		global $post;
 
 		// Nonce field to validade form request cam from current site
@@ -71,37 +103,10 @@ function cta_rockcontent_init() {
 	    echo '<p>'. __( 'Link de para onde o CTA deve redirecionar o usuário. Informe a url completa. ex: https://blog.com.br/meu-ebook', 'cta-rockcontent' ) .'</p>';
 	}
 
-
-	//Adding the [cta-banner] shortcode
-	add_shortcode('cta-banner', 'cta_rockcontent_shortcode_show_html');
-	//Shortcode function to return banner in html elements
-	function cta_rockcontent_shortcode_show_html( $atts ){
-		//Verify if array $atts has the 'id' param, and if is not empty and has a numeric value
-		if(isset($atts['id']) && !empty($atts['id']) && is_numeric($atts['id'])) { 
-			$cta = get_post($atts['id']);
-			if($cta){
-				//Check if the post has thumbnail
-				$featured_image = has_post_thumbnail($cta) ? get_the_post_thumbnail( $cta, 'cta-banner-size' ) : '';
-				
-				//Return CTA's banner with link, or just banner image
-				$cta_banner_url = get_post_meta( $cta->ID, 'cta-banner-url', true);
-
-				if(!is_null($cta_banner_url)){
-					//Verify if the CTA has title to use it on link title attribute
-					$cta_title = !empty($cta->post_title)?' title="'.$cta->post_title.'" ':'';
-					//And then return link html element
-					return '<a href="' . $cta_banner_url . '" ' . $cta_title . ' >'. $featured_image . '</a>';
-
-				} else return $featured_image; //return just the featured image
-
-			} else return __("#ERROR: No CTA's has found with id=" . $atts['id'], 'cta-rockcontent');
-
-		} else return;
-	}
-
-	// CTA Banner Metabox Save Data Function
-	add_action( 'save_post', 'cta_banner_save_meta' );
-	function cta_banner_save_meta( $post_id ) {
+	/***************************************** 
+	   CTA Banner Metabox Save Data Function
+	*****************************************/
+	public function cta_banner_save_meta( $post_id ) {
 		// Return if the user doesn't have edit permissions.
 		if ( ! current_user_can( 'edit_post', $post_id ) ) {
 			return $post_id;
@@ -127,7 +132,7 @@ function cta_rockcontent_init() {
 	    }
 
 
-	    // Sanitize the user input.
+	    // Sanitize the user input
 	    $url = sanitize_text_field( $_POST['cta_banner_url_text_input'] );
 	    //Verify if is a valid url
 	    if(filter_var($url, FILTER_VALIDATE_URL)){
@@ -144,5 +149,40 @@ function cta_rockcontent_init() {
 	    } else return $post_id; // Not valid URL
 	    	
 	}
+
+	
+	/***********************************************************************
+		Shortcode - Shortcode function to return banner in html elements
+	************************************************************************/
+	public function cta_rockcontent_shortcode_show_html( $atts ){
+		//Verify if array $atts has the 'id' param, and if is not empty and has a numeric value
+		if(isset($atts['id']) && !empty($atts['id']) && is_numeric($atts['id'])) { 
+			$cta = get_post($atts['id']);
+			if($cta){
+				//Check if the post has thumbnail
+				$featured_image = has_post_thumbnail($cta) ? get_the_post_thumbnail( $cta, 'cta-banner-size' ) : '';
+				
+				//Return CTA's banner with link, or just banner image
+				$cta_banner_url = get_post_meta( $cta->ID, 'cta-banner-url', true);
+
+				if(!is_null($cta_banner_url)){
+					//Verify if the CTA has title to use it on link title attribute
+					$cta_title = !empty($cta->post_title)?' title="'.$cta->post_title.'" ':'';
+					//And then return link html element
+					return '<a href="' . $cta_banner_url . '" ' . $cta_title . ' >'. $featured_image . '</a>';
+
+				} else return $featured_image; //return just the featured image
+
+			} else return __("#ERROR: No CTA's has found with id=" . $atts['id'], 'cta-rockcontent');
+
+		} else return;
+	}
+
+}//End of Class CTA_RockContent
+
+if(class_exists('CTA_RockContent'))
+{
+	//Initialize a instace of CTA_RockContent Class
+	$ctaRockContent = new CTA_RockContent();
 }
 ?>
